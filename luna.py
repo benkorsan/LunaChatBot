dfa52c8a1683c09508a7feef1dc192ce7d3595d2
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 02:53:51 +0300

    Update luna.py

--- luna.py
+++ luna.py
@@ -81,7 +81,6 @@ async def chat(_, message):
 
 
 async def chat(_, message):
-async def ai(_, message: Message):
     if message.reply_to_message and message.reply_to_message.from_user.id 
         ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={message.text}", timeout=3).json()["response"]
         print(ai_gen)
