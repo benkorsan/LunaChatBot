d8346d3943c5e6960b0406ad28e2d1c84028c450
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 12:33:06 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID"))
+TOKEN = getenv("TOKEN")
+OWNERID = int(getenv("OWNERID"))  # your userid
+BOT_ID = int(getenv("BOT_ID"))
+DB_URL = getenv("DB_URL")
+DB_NAME = getenv("DB_NAME")
+
+neko = Client(name="aichan", api_hash=API_HASH, api_id=API_ID,
+              bot_token=TOKEN,)
