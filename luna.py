b60ed68c9d6eb99d217b1a0463301da21ed1553e
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 03:37:51 +0300

    Update luna.py

--- luna.py
+++ luna.py
@@ -82,7 +82,7 @@ async def chat(_, message):
 @luna.on_message(filters.text, group=100)
 async def ai(_, message: Message):
     if message.reply_to_message and message.reply_to_message.from_user.id == BOT_ID:
-        ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={message.text}", timeout=5).json()["response"]
+        ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={message.text}", timeout=2).json()["response"]
         print(ai_gen)
         await neko.send_message(chat_id=message.chat.id ,text=ai_gen , reply_to_message_id=message.id)
 
@@ -90,7 +90,7 @@ async def ai(_, message: Message):
 @luna.on_message(filters.command(commands=["harley"] , prefixes="@"))
 async def username(_, message: Message):
     fixed_text = message.text.replace("Sa", "")
-    ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={fixed_text}", timeout=5).json()["response"]
+    ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={fixed_text}", timeout=2).json()["response"]
     print(ai_gen)
     await luna.send_message(chat_id=message.chat.id ,text=ai_gen, reply_to_message_id=message.id)
 
