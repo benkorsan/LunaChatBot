0bff979f6074c4f2b851f313dcf8cd4d1e10c17a
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 13:27:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import re
-import os
+from config import neko
 from pyrogram import filters, Client 
 from pyrogram.types import Message
 import requests
-from asyncio import gather, get_event_loop, sleep
-from pyrogram import filters, Client 
-from pyrogram.types import Message
-from aiohttp import ClientSession
-from pyrogram import Client, filters, idle
+from config import neko, BOT_ID
 
 
-is_config = os.path.exists("config.py")
-
-if is_config:
-    from config import *
-else:
-    from sample_config import *
-
-neko = Client(
-    ":memory:",
-    bot_token=bot_token,
-    api_id=6,
-    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
-)
-
-
-bot_id = int(bot_token.split(":")[0])
 
 @neko.on_message(filters.text, group=100)
 async def ai(_, message: Message):
