04506c5bce7c7b3b654d73364cb918fdce50eafa
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 12:42:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,28 @@
+
 from pyrogram import Client
 from os import getenv
 from dotenv import load_dotenv
 
+HEROKU = True # Make it False if you're not deploying on heroku.
 
+if HEROKU:
+    from os import environ
+
+    bot_token = environ["bot_token"]
+    ARQ_API_KEY = environ["ARQ_API_KEY"]
+    LANGUAGE = environ["LANGUAGE"]
+    BOT_ID= environ["BOT_ID"] 
+# NOTE Fill this if you are not deploying on heroku.
+if not HEROKU:
+    BOT_ID="5330559241" 
+    bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
+    ARQ_API_KEY = "Get this from @ARQRobot"
+# List of supported languages >>
+# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
+    LANGUAGE = "en"
+
+# Leave it as it is
+ARQ_API_BASE_URL = "https://arq.hamker.in"
 load_dotenv()
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID"))
