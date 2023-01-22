fe4e40d70a269789158d4275738a0be581eb500a
benkorsan <103997850+benkorsan@users.noreply.github.com>
2023-01-22 03:16:23 +0300

    Update luna.py

--- luna.py
+++ luna.py
@@ -80,11 +80,17 @@ async def chat(_, message):
 
 
 
-    
+    @neko.on_message(filters.text, group=100)
+async def ai(_, message: Message):
+    if message.reply_to_message and message.reply_to_message.from_user.id == bot_id:
+        ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={message.text}", timeout=5).json()["response"]
+        print(ai_gen)
+        await neko.send_message(chat_id=message.chat.id ,text=ai_gen , reply_to_message_id=message.id)
+
 
 @luna.on_message(filters.command(commands=["harley"] , prefixes="@"))
 async def username(_, message: Message):
-    fixed_text = message.text.replace("Ha ", "Sa")
+    fixed_text = message.text.replace("Sa", "")
     ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={fixed_text}", timeout=5).json()["response"]
     print(ai_gen)
     await luna.send_message(chat_id=message.chat.id ,text=ai_gen, reply_to_message_id=message.id)
